ee35c8619f294bad092fb81666ea2fa11caa5086
leeozzi <lstory1997l@gmail.com>
2025-05-24 13:26:53 +0900

    [언지] 회원가입 프론트 구현

--- backend/src/main/java/com/ssafy/pjtFinal/config/WebConfig.java
+++ backend/src/main/java/com/ssafy/pjtFinal/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.ssafy.pjtFinal.interceptor.SessionInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -27,5 +28,14 @@ public class WebConfig implements WebMvcConfigurer {
                         "/api/user/**",      // 단건조회, 리스트 조회 등
                         "/api/user/login"    // 로그인
                 );
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
